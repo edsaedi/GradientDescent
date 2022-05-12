0bdbff86ff3f64273694b95993aaf926a3fc8bd1
GMR Student <gmrstudent@outlook.com>
2022-05-11 18:24:13 -0700

    Continue working on the and gate

---
+++ GradientDescent/AndGate/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using PerceptronWithGradientDescent;
+
+
+namespace AndGate
+{
+    class Program
+    {
+        public static double MeanSquaredError(double output, double desiredOutput)
+        {
+            return Math.Pow((desiredOutput - output), 2);
+        }
+
+        public static (double[][] inputs, double[] outputs) AndInOut()
+        {
+            double[][] inputs = new double[][] { new double[] { 0, 0 }, new double[] { 1, 0 }, new double[] { 0, 1 }, new double[] { 1, 1 } };
+            double[] outputs = new double[] { 0, 0, 0, 1 };
+            return (inputs, outputs);
+        }
+
+        public static double BinaryStep(double input)
+        {
+            if (input >= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            //creating a perceptron
+            GradientPerceptron gate = new GradientPerceptron(amountOfInputs: 2, learningRate: , new ActivationFunction(BinaryStep, derivative: x => 0), MeanSquaredError);
+
+            //Setting random values for our weights and bias
+            gate.Randomize(random, -1, 1);
+
+            (double[][] inputs, double[] outputs) = AndInOut();
+            double currentError = gate.GetError(inputs, outputs);
+            //while (currentError > 0)
+            //{
+            //    currentError = gate.TrainWithHillClimbing(inputs, outputs, currentError);
+            //}
+
+            while (currentError > 0)
+            {
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    Console.Write("Inputs: ");
+                    for (int j = 0; j < inputs[i].Length; j++)
+                    {
+                        if (j != 0)
+                        {
+                            Console.Write(", ");
+                        }
+                        Console.Write(inputs[i][j]);
+                    }
+
+                    Console.Write(" Output: " + gate.Compute(inputs[i]) + "             ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Error: " + Math.Round(currentError, 3) + "           ");
+                currentError = gate.TrainWithHillClimbing(inputs, outputs, currentError);
+            }
+
+            Console.WriteLine("Final Error: " + Math.Round(currentError, 3) + "");
+            currentError = gate.TrainWithHillClimbing(inputs, outputs, currentError);
+
+            //for (int i = 0; i < inputs.Length; i++)
+            //{
+            //    Console.Write("Inputs: ");
+            //    for (int j = 0; j < inputs[i].Length; j++)
+            //    {
+            //        if (j != 0)
+            //        {
+            //            Console.Write(", ");
+            //        }
+            //        Console.Write(inputs[i][j]);
+            //    }
+
+            //    Console.Write(" Output: " + gate.Compute(inputs[i]) + "             ");
+            //    Console.WriteLine();
+            //}
+            Console.WriteLine("Complete");
+            //write the four test cases and train perceptron until it's right
+        }
+
+    }
+}
--- GradientDescent/PerceptronWithGradientDescent/GradientPerceptron.cs
+++ GradientDescent/PerceptronWithGradientDescent/GradientPerceptron.cs
@@ -22,7 +22,7 @@ namespace PerceptronWithGradientDescent
         }
 
         //Randomize is in base class
-        
+
         //Not needed because there is a shared 
         //public double Compute(double[] inputs)
         //{
@@ -36,18 +36,6 @@ namespace PerceptronWithGradientDescent
 
         //Get error is in base class
 
-        void PartialWeight()
-        {
-            //derivative of activation function is easy
-            activationFunction.Derivative(/*input*/);
-        }
-
-        void PartialBias()
-        {
-            //derivative of activation function is easy
-            activationFunction.Derivative(/*input*/);
-        }
-
         public double Train(double[] inputs, double desiredOutput)
         {
             /*trains the perceptron using gradient descent for one iteration and returns the error */
@@ -56,13 +44,16 @@ namespace PerceptronWithGradientDescent
             double output = activationFunction.Function(computed);
             double error = errorFunction.Function(output, desiredOutput);
 
-            //double learningRate 
+            //Computes weightChangeScalar = e(o,d) * a'(z) * -LearningRate;
+            //learning rate is inputed
+            double weightChangeScalar = errorFunction.Derivative(output, desiredOutput) * activationFunction.Derivative(computed) * -LearningRate;
 
-
-
-            
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] += weightChangeScalar * inputs[i];
+            }
             //LearningRate * (-1) * (partial derivative)
-
+            bias += weightChangeScalar;
 
             return error;
         }
@@ -70,7 +61,38 @@ namespace PerceptronWithGradientDescent
         public double Train(double[][] inputs, double[] desiredOutput)
         {
             /*batch trains the perceptron using gradient descent for one iteration and returns the error */
-            return 0;
+            double[] computed = base.Compute(inputs);
+
+            double[] output = new double[inputs.Length];
+            for (int i = 0; i < output.Length; i++)
+            {
+                output[i] = activationFunction.Function(computed[i]);
+            }
+
+            double error = GetError(inputs, desiredOutput);
+
+            for (int i = 0; i < desiredOutput.Length; i++)
+            {
+                double weightChangeScalar = errorFunction.Derivative(output[i], desiredOutput[i]) * activationFunction.Derivative(computed[i]) * -LearningRate;
+
+                for (int j = 0; j < weights.Length; j++)
+                {
+                    weights[j] += weightChangeScalar * inputs[i][j];
+                }
+                bias += weightChangeScalar;
+            }
+            
+            return error;
+        }
+
+        public double TrainForIterations(double[][] inputs, double[] desiredOutputs, int trainingIterations)
+        {
+            double error = 0;
+            for (int i = 0; i < trainingIterations; i++)
+            {
+                error = Train(inputs, desiredOutputs);
+            }
+            return error;
         }
     }
 }
